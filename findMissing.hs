f09cba731fa0d783b6800095b05d250a9f8315d1
Baojun Wang <wangbj@gmail.com>
2016-03-22 16:08:01 -0700

    Update findMissing.hs

--- findMissing.hs
+++ findMissing.hs
@@ -5,10 +5,12 @@ import Control.Monad
 numDigits 0 = 0
 numDigits x = 1 + numDigits ( x `div` 10)
 
-readint = fst . fromJust . C.readInt
+readint = fst . fromJust . C.readInteger
 nexts st k s
   | len < digits = Nothing
-  | len == digits = st `mplus` if readint s == 2+k then Just (1+k) else Nothing
+  | len == digits = case st of
+      Just x -> if readint s /= 1 + k then Nothing else Just x
+      Nothing -> if readint s == 2 + k then Just (1+k) else Nothing
   | x == 1 + k = nexts st x s'
   | x == 2 + k = case st of
       Nothing -> nexts (Just (1+k)) (2+k) s'
@@ -24,3 +26,5 @@ findMissing s = go 1
           | k > (C.length s) `div` 2 = Nothing
           | otherwise = nexts Nothing (readint s1) s' `mplus` go (1+k)
             where (s1, s') = C.splitAt k s
+
+main = C.getContents >>=  mapM_ (print . findMissing) . C.words
