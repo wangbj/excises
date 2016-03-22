9b90138fe61508c73b4fe2f03fb52ed0cee2ce07
Baojun Wang <wangbj@gmail.com>
2016-03-22 15:36:54 -0700

    Create findMissing.hs
    Finding missing number in a string, such as:
    
    "13141617" -> 15
    
    Pre-condition: there is always one missing number between.

---
+++ findMissing.hs
@@ -0,0 +1,29 @@
+import qualified Data.ByteString.Char8 as C
+import Data.Maybe
+import Control.Monad
+
+numDigits 0 = 0
+numDigits x = 1 + numDigits ( x `div` 10)
+
+readint = fst . fromJust . C.readInt
+
+nexts st k s
+  | len < digits = Nothing
+  | len == digits = case st of
+      Just x -> Just x
+      Nothing -> if (readint s == (2+k)) then Just (1+k) else Nothing
+  | x == 1 + k = nexts st x s'
+  | x == 2 + k = case st of
+      Nothing -> nexts (Just (1+k)) (2+k) s'
+      Just _ -> Nothing
+  | x /= (1+k) && x /= (2+k) = Nothing
+  where digits = numDigits (1+k)
+        len = C.length s
+        (s1, s') = C.splitAt digits s
+        x = readint s1
+
+findMissing s = go 1
+  where go k
+          | k > (C.length s) `div` 2 = Nothing
+          | otherwise = nexts Nothing (readint s1) s' `mplus` go (1+k)
+            where (s1, s') = C.splitAt k s
