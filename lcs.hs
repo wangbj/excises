bcbe0d8b6e81da254321034e133b739859af7413
Baojun Wang <wangbj@gmail.com>
2016-03-29 23:06:11 -0700

    Update lcs.hs

--- lcs.hs
+++ lcs.hs
@@ -1,14 +1,11 @@
 import qualified Data.ByteString.Char8 as C
 import Data.Array.Unboxed
-import Control.Arrow
-import Data.Maybe
 import System.Environment
 import System.IO
 
-getone s = (s1, s2)
-  where (s1, s') = C.break (== ';') s
-        s2 = C.tail s'
-getinputs = map getone . C.lines
+getinputs = map getline . C.lines
+  where getline s = (s1, s2)
+          where (s1:s2:_) = C.split ';' s
 
 data P = P {-# UNPACK #-} !Int String
 
@@ -16,14 +13,14 @@ instance Eq P where
   (P x _) == (P y _) = x == y
 
 instance Ord P where
-  compare (P x s) (P y t) = compare x y
+  compare (P x _) (P y _) = compare x y
 
 instance Show P where
   show (P _ s) = reverse s
 
 lcs s t = cache ! (len1, len2)
-  where u1 = listArray (1, len1) (C.unpack s) :: UArray Int Char
-        u2 = listArray (1, len2) (C.unpack t) :: UArray Int Char
+  where u1   = listArray (1, len1) (C.unpack s) :: UArray Int Char
+        u2   = listArray (1, len2) (C.unpack t) :: UArray Int Char
         len1 = C.length s
         len2 = C.length t
         go i j
@@ -33,7 +30,6 @@ lcs s t = cache ! (len1, len2)
         cache = listArray ((0,0), (len1, len2)) [ go x y | x <- [0..len1], y <- [0..len2]] :: Array (Int, Int) P
         append c (P x s) = P (succ x) (c:s)
 
-mainloop handle = C.hGetContents handle >>= mapM_ (print . uncurry lcs) . getinputs
-main = do
-  fname:_ <- getArgs
-  withFile fname ReadMode mainloop
+main = do  fname:_ <- getArgs
+           withFile fname ReadMode mainloop
+       where mainloop handle = C.hGetContents handle >>= mapM_ (print . uncurry lcs) . getinputs
